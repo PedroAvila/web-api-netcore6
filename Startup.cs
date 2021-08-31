d4518b48d98627d5b6177ea5f311897cc4ed471f
PedroAvila <minimainor_austin@hotmail.com>
2021-08-31 09:41:58 -0500

    primer commit

---
+++ Startup.cs
@@ -0,0 +1,84 @@
+﻿
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using WebApiAutores.Filtros;
+using WebApiAutores.Middlewares;
+using WebApiAutores.Servicios;
+
+namespace WebApiAutores;
+public class Startup
+{
+    public Startup( IConfiguration configuration )
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers( opciones => {
+            opciones.Filters.Add(typeof(FiltroDeExcepcion));
+        } ).AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("defaultConnection")));
+
+        services.AddTransient<IServicio, ServicioA>();
+        //services.AddTransient<ServicioA>();
+        services.AddTransient<ServicioTransient>();
+        services.AddScoped<ServicioScoped>();
+        services.AddSingleton<ServicioSingleton>();
+        services.AddTransient<MiFiltroDeAccion>();
+        services.AddHostedService<EscribirEnArchivo>();
+
+        services.AddResponseCaching();
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();
+
+
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new() { Title = "WebApiAutores", Version = "v1" });
+        });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
+    {
+        //app.UseMiddleware<LoguearRespuestaHTTPMiddleware>();
+
+        app.UseLoguearRespuestaHTTP();
+
+        app.Map("/ruta1", app =>
+        {
+            app.Run(async contexto =>
+            {
+                await contexto.Response.WriteAsync("Estoy interceptando la tubería");
+            });
+        });
+
+        
+
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApiAutores v1"));
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseResponseCaching();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+
+}
